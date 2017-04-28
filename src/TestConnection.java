638afe867dfb4c4f0fbf5bb4437cf4ef169b89df
Nathan Dunn <ndunn@me.com>
2017-04-27 17:31:24 -0700

    updated pom and connection data

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -1,7 +1,27 @@
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
 /**
  * Created by nathandunn on 4/27/17.
  */
 public class TestConnection {
 
+    public static void main(String args[]) {
+
+        System.out.println("starting the client");
+        CloseableHttpClient httpclient = HttpClients.createDefault(); // aka connection
+        HttpGet httpget = new HttpGet("https://google.com");
+        try {
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            System.out.println("response: " + response.getStatusLine().getStatusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("no error, yeah!");
+    }
 
 }
